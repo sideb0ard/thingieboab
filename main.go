7d7cb351c7c50b65dd243290d9a29b4d9f1d91d5
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2014-11-04 21:02:34 -0800

    simplify

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"database/sql"
 	"flag"
 	"fmt"
 	"github.com/coopernurse/gorp"
 	_ "github.com/lib/pq"
+	"io/ioutil"
 	"log"
 	"net"
-	//"os/exec"
-	//"time"
+	"net/http"
+	"strings"
 )
 
 const (
@@ -23,13 +26,10 @@ func main() {
 	var debug = flag.Bool("d", false, "debug - whether to print copious what-i-m-doing messages")
 	flag.Parse()
 
-	var keywurds Keywurds
-
 	var b Bot
 	b.Name = "AIGOR"
 	b.Mood = 100
 	b.Debug = *debug
-	b.innit(&keywurds)
 
 	db, err := sql.Open("postgres", "user=thingie dbname=thingie sslmode=disable")
 	err = db.Ping()
@@ -48,7 +48,7 @@ func main() {
 	}
 	conns := clientConns(l)
 	for {
-		go b.talkPerson(<-conns, keywurds, dbmap)
+		go b.talkPerson(<-conns, dbmap)
 	}
 }
 
@@ -69,4 +69,71 @@ func clientConns(l net.Listener) chan net.Conn {
 		}
 	}()
 	return ch
+}
+
+func (b Bot) talkPerson(conn net.Conn, dbmap *gorp.DbMap) {
+	bu := bufio.NewReader(conn)
+	p := &Thing{}
+
+	conn.Write([]byte(">** HUllo. I am " + b.Name + "\n"))
+
+	// first run - get some initial infozzzz...
+	if len(p.Name) == 0 {
+		conn.Write([]byte(">What is your name*?\n"))
+		line, err := bu.ReadBytes('\n')
+		if err != nil {
+			fmt.Println("Errzzz reading :", err.Error())
+		}
+		nom := strings.TrimSpace(string(line))
+		err = dbmap.SelectOne(p, "select * from thing where name=$1", nom)
+		if err != nil {
+			fmt.Println(nom, "doesnt exist - now creating..")
+			conn.Write([]byte("\n>Please to meet ya " + nom + ". Wha's up?\n"))
+			p.Name = nom
+			err = dbmap.Insert(p)
+			if err != nil {
+				fmt.Println("ERrrr inserting to DB:", err.Error())
+			}
+		} else {
+			fmt.Println("woop! exists")
+			conn.Write([]byte("\n>hey " + nom + ", I know you!. How are ya?\n"))
+		}
+	}
+	for {
+		line, err := bu.ReadBytes('\n')
+		if err != nil {
+			fmt.Println("Errzzz reading :", err.Error())
+			break
+		}
+		reply := b.transform(string(line))
+		conn.Write([]byte("\n>" + reply + "\n"))
+	}
+}
+
+func (b Bot) dream() {
+	fmt.Println("electric sheepzzzzzzz")
+}
+
+func (b Bot) transform(s string) string {
+	url := "http://localhost:5000/"
+	var jsonnn = "{\"wurds\": \"" + strings.TrimSpace(s) + "\"}"
+	fmt.Println(jsonnn)
+	var jsonStr = []byte(jsonnn)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("X-Sideb0ard-Service", "v1")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+	return string(body)
+
 }
